f02d7dd375e903e4912af9828437fcdcb5ea936a
June Torres <juniortorr77@gmail.com>
2023-09-17 16:27:23 -0700

    Add functionality to button add newgrid function which takes the number input and then turns the new grid into that number multiplied on itself.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const content = document.querySelector('.content');
+const newGridBtn = document.querySelector('.newGridBtn');
 
-for(let i=1; i<17; i++) {
-    let gridSq = document.createElement('div');
-    gridSq.classList.add('gridUnit')
-    content.append(gridSq);
+
+
+function newGrid() {
+    let newNum = prompt('Pick a new number for your grid!', '');
+    while(content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+    newNum = parseInt(newNum);
+    let newWidth = 100/newNum + '%';
+    console.log(newWidth);
+    generateGrid(newNum, newWidth);
 }
+
+
+function generateGrid(gridNum, newUnitWidth = '6.25%') {
+    for(let i=1; i<=gridNum**2; i++) {
+        let gridSq = document.createElement('div');
+        gridSq.classList.add('gridUnit')
+        // gridSq.style.width = newUnitWidth;
+        // gridSq.style.height = newUnitWidth;
+        content.append(gridSq);
+        gridSq.addEventListener('mouseover', () => {
+            gridSq.style.backgroundColor = 'blue';
+            setTimeout(() => { gridSq.style.backgroundColor = ''}, 2000);
+        });
+    }
+}
+
+
+
+generateGrid(16);
+
+newGridBtn.addEventListener('click', ()=> {
+    newGrid();
+});
